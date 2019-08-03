fdde028bbea8fd2a09a7ab251ebbf0d80211942d
Srikalyan Megati <kalmegati@gmail.com>
2019-08-03 16:53:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,12 @@ def long_planeteer_calls(kiai)
   kiai.any? { |memo| memo.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(fruit_and_cheese)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  fruit_and_cheese.detect { |memo|
+    cheese_types.length.times { |index|
+      memo == cheese_types[index]
+    }
+  }
 end
