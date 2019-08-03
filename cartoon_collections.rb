7bee20169cde029fbbd4f13e3e246f06cf23efae
Srikalyan Megati <kalmegati@gmail.com>
2019-08-03 17:13:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,14 +15,24 @@ def find_the_cheese(fruit_and_cheese)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   fruit_and_cheese.detect { |memo|
-    counter = 0
-    while counter < cheese_types.length do
-      if memo = cheese_types[counter]
-        true
-        counter = cheese_types.length
-      else
-        counter += 1
-      end
-    end  
+    memo = cheese_types[0] ||
+    memo = cheese_types[1] ||
+    memo = cheese_types[2]
   }
 end
+
+#def find_the_cheese(fruit_and_cheese)
+#  # the array below is here to help
+#  cheese_types = ["cheddar", "gouda", "camembert"]
+#  fruit_and_cheese.detect { |memo|
+#    counter = 0
+#    while counter < cheese_types.length do
+#      if memo = cheese_types[counter]
+#        true
+#        counter = cheese_types.length
+#      else
+#        counter += 1
+#      end
+#    end  
+#  }
+#end
