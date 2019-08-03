82a27b66255bad82921e2938cf803c3a7367094b
Srikalyan Megati <kalmegati@gmail.com>
2019-08-03 16:28:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,3 @@ def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
-
-quartet = "magic, madness, heaven, sin"
-
-p quartet[3]
