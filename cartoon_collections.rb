45061723f21851e95a151c5e372ad968a2873e38
Srikalyan Megati <kalmegati@gmail.com>
2019-08-03 16:34:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(dwarves)
-  dwarves.each_with_index { |memo, index|
+  dwarves.each_with_index { |memo, index| p (index + "")
 end
 
 def summon_captain_planet# code an argument here
